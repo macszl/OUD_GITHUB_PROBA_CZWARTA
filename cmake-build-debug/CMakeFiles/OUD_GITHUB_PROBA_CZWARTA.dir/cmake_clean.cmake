2b511b01179aec0bc82101b5e80c1a72ffdde916
macszl <235989@edu.p.lodz.pl>
2021-02-01 17:17:14 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OUD_GITHUB_PROBA_CZWARTA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OUD_GITHUB_PROBA_CZWARTA.dir/main.c.obj"
+  "OUD_GITHUB_PROBA_CZWARTA.exe"
+  "OUD_GITHUB_PROBA_CZWARTA.exe.manifest"
+  "OUD_GITHUB_PROBA_CZWARTA.pdb"
+  "libOUD_GITHUB_PROBA_CZWARTA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OUD_GITHUB_PROBA_CZWARTA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
